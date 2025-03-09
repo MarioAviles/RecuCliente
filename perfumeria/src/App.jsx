cc2c9f866489d2c092786829a15ec78cd72f3df6
MarioAviles <4150123@alu.murciaeduca.es>
2025-03-09 10:06:12 +0100

    fcafc

--- perfumeria/src/App.jsx
+++ perfumeria/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Cabecera from './componentes/Cabecera/Cabecera'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Cabecera></Cabecera>
+
+      {/* <Routes>
+
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/juego/:id" element={<Detalle></Detalle>}></Route>
+
+      </Routes>
+
+      <Suspense fallback={"Cargando pie de pagina"}>
+        <LazyLoad offset={250}>
+          <Pie></Pie>
+        </LazyLoad>
+      </Suspense>
+      <ScrollUpDown /> */}
     </>
   )
 }
