54d19894b93f859a3d40f3e1dfe7e48d04045e66
MarioAviles <4150123@alu.murciaeduca.es>
2025-03-09 09:42:02 +0100

    fafcas

--- nintendo/src/componentes/Buscador/Buscador.jsx
+++ nintendo/src/componentes/Buscador/Buscador.jsx
@@ -1,13 +1,18 @@
 import './Buscador.css'
 
 const Buscador = (props) => {
+
+    function actualizarBusqueda(event) {
+        props.setKeyword(event.target.value)
+    }
+
     return (
         <div className='buscador'>
             <input
                 type="text"
                 placeholder="Buscar juego..."
-                value={props.skeyword}
-                onChange={(e) => props.setKeyword(e.target.value)}
+                value={props.keyword}
+                onChange={actualizarBusqueda}
             />
             <button onClick={props.aplicarBusqueda}>Buscar</button>
         </div>
